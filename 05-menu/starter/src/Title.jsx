96e706bb227eba1c3840341283fff0cc679fe1a7
atilgan <a.sahinulasan@gmail.com>
2023-08-14 23:32:03 +0300

    Menu

---
+++ 05-menu/starter/src/Title.jsx
@@ -0,0 +1,9 @@
+const Title = ({ text }) => {
+  return (
+    <div>
+      <h2>{text || 'Default Title'}</h2>
+      <div className='title-underline'></div>
+    </div>
+  );
+};
+export default Title;
