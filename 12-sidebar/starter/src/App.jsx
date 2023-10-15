3aef791712a6d684cd50d49d33549897994a768b
atilgan <a.sahinulasan@gmail.com>
2023-10-15 17:01:57 +0300

    Sidebar

---
+++ 12-sidebar/starter/src/App.jsx
@@ -0,0 +1,12 @@
+import Modal from "./Modal";
+import Sidebar from "./Sidebar";
+import Home from "./Home";
+
+const App = () => {
+  return <>
+    <Home/>
+    <Modal/>
+    <Sidebar/>
+  </>;
+};
+export default App;
